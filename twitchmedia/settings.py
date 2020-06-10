b5ec3110ea4e2bd468bb6be723663ec0d4b71b6e
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-10 19:58:06 +0300

    Fix allowed hosts

--- twitchmedia/settings.py
+++ twitchmedia/settings.py
@@ -30,7 +30,7 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "https://twitchmedia.herokuapp.com/"
+    "twitchmedia.herokuapp.com"
 ]
 
 # Application definition
