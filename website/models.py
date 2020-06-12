3829676ef9323f2a35825c6c08a1325ebfaabb74
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-12 17:44:50 +0300

    add method for update and create profile

--- website/models.py
+++ website/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -8,6 +10,17 @@ class Profile(models.Model):
     tg_name = models.TextField("Имя пользователя Telegram", max_length=50)
 
     # subscribe_list=  Добавить список подписок
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
     class Meta:
         verbose_name = "Профиль"
