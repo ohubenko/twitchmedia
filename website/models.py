833205ac9a2e9cb153f0cae28fd6fd1024a8f53d
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-12 16:51:39 +0300

    Change model for profile

--- website/models.py
+++ website/models.py
@@ -6,8 +6,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     tg_chat_id = models.PositiveIntegerField(max_length=20)
     tg_name = models.TextField(max_length=50)
+
     # subscribe_list=  Добавить список подписок
 
     class Meta:
-        pass
-
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
