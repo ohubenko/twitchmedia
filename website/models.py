abec8cb5bdebd4923a5b16d624b40b504006be7a
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-12 16:28:03 +0300

    Add REST api auth, and do setings for this

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tg_chat_id = models.PositiveIntegerField(max_length=20)
+    tg_name = models.TextField(max_length=50)
+    # subscribe_list=  Добавить список подписок
+
+    class Meta:
+        pass
+
