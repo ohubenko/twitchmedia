f666ac304894974c7151b3620bfda64fa6f9ecac
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-13 16:07:08 +0300

    Reg vk,twitch model in admin

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
-from .models import Profile, Article
+from .models import Profile, Article, TwitchProfile, VKProfile
 
 admin.site.register(Profile)
 admin.site.register(Article)
+admin.site.register(TwitchProfile)
+admin.site.register(VKProfile)
