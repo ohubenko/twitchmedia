878d20401a350dc84fde5d4ddf7f0763ca98bdaa
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-13 17:41:32 +0300

    Crate views when profile not find

--- website/views.py
+++ website/views.py
@@ -18,9 +18,12 @@ class ProfileView(APIView):
     """Вывод пользователя"""
 
     def get(self, requset, chat_id):
-        profile = Profile.objects.get(tg_chat_id=chat_id)
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
+        try:
+            profile = Profile.objects.get(tg_chat_id=chat_id)
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        except Profile.DoesNotExist:
+            return Response(status=404)
 
 
 class ProfileCreateView(APIView):
