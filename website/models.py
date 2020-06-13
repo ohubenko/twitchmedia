fa1574b30918a32248548a2901314f999276f42c
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-13 16:06:26 +0300

    Add modles for VK,Twitch profile

--- website/models.py
+++ website/models.py
@@ -4,12 +4,47 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    tg_chat_id = models.IntegerField("ID чата Telegram")
-    tg_name = models.TextField("Имя пользователя Telegram", max_length=50)
+class TwitchProfile(models.Model):
+    name_twitch = models.TextField(unique=True, max_length=15, primary_key=True)
+    url = models.URLField(unique=True)
+    oidc_token = models.TextField(unique=True, max_length=128, default=None)
 
+    # sub_list = Список подписок
+
+    class Meta:
+        verbose_name = "Twitch профиль"
+        verbose_name_plural = "Twitch профили"
+        # abstract = True
+
+
+class VKProfile(models.Model):
+    vk_tokken = models.TextField(unique=True, max_length=128)
+    vk_page_URL = models.URLField(unique=True)
+    # vk_group_list = Список групп
+    # vk_streamer_group = Если стример то его группа
+    vk_key_group = models.TextField(max_length=21)
+
+    class Meta:
+        verbose_name = "VK профиль"
+        verbose_name_plural = "VK профили"
+        # abstract = True
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True)
+    tg_chat_id = models.IntegerField("ID чата Telegram",
+                                     unique=True)
+    tg_name = models.TextField("Имя пользователя Telegram",
+                               max_length=50)
+    tg_auth_key = models.TextField(max_length=10, default=tg_chat_id)
+    vk_profile = models.OneToOneField(VKProfile, on_delete=models.CASCADE, default=None)
+    twitch_profile = models.OneToOneField(TwitchProfile, on_delete=models.CASCADE, default=None)
+
+    # vk_list = #Добавить список групп на которые он подписан
     # subscribe_list=  Добавить список подписок
+
     class Meta:
         verbose_name = "Профиль"
         verbose_name_plural = "Профили"
