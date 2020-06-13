18a24273954d4517c7e8c5326c9463fd6a2d31bf
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-13 12:50:23 +0300

    Change settings, add local host and change how take env secret key

--- twitchmedia/settings.py
+++ twitchmedia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = str(os.environ.get('DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -31,7 +31,7 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = [
     'localhost',
-    '"127.0.0.1',
+    '127.0.0.1',
     'twitchmedia.herokuapp.com',
     'twitch-media.me',
     'www.twitch-media.me',
