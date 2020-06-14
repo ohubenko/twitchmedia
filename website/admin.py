389c1b1d536da229a4d09abd7f8d679d237c24c5
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-14 22:37:39 +0300

    Register models

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,9 @@
-
 from django.contrib import admin
+from .models import TelegramProfile, TwitchProfile, VKGroup, VKProfile, Subscribe, Profile
 
-
+admin.register(Profile)
+admin.register(TelegramProfile)
+admin.register(TwitchProfile)
+admin.register(VKGroup)
+admin.register(VKProfile)
+admin.register(Subscribe)
