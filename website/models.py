2fd96bebcfc8a72ee4b5abacea31f75a9e159655
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-15 15:31:59 +0300

    Final change model

--- website/models.py
+++ website/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class TelegramProfile(models.Model):
-    chat_id = models.SmallIntegerField(verbose_name="ID чата с ботом", unique=True, primary_key=True)
+    chat_id = models.IntegerField(verbose_name="ID чата с ботом", unique=True, primary_key=True)
     username = models.TextField(verbose_name="Имя пользователя")
     name = models.TextField(verbose_name="Имя")
 
@@ -18,8 +16,9 @@ class TelegramProfile(models.Model):
 
 
 class TwitchProfile(models.Model):
-    username = models.TextField(max_length=15, verbose_name="Имя пользователя", primary_key=True)
-    url = models.URLField(verbose_name="Ссылка на профиль Twitch")
+    username = models.TextField(max_length=15, verbose_name="Имя пользователя", primary_key=True, unique=True)
+    url = models.URLField(verbose_name="Ссылка на профиль Twitch", default="https://twitch.tv/" + str(username),
+                          blank=True)
     oidc_token = models.TextField(verbose_name="OIDC токен", blank=True, unique=True)
 
     def __str__(self):
@@ -56,10 +55,30 @@ class VKProfile(models.Model):
         verbose_name_plural = "Профили VK"
 
 
+class Profile(models.Model):
+    tg_profile = models.OneToOneField(TelegramProfile, on_delete=models.CASCADE, verbose_name="Профиль Telegram",
+                                      blank=True)
+    vk_profile = models.OneToOneField(VKProfile, on_delete=models.CASCADE, verbose_name="Профиль VK", blank=True,
+                                      null=True)
+    twitch_profile = models.OneToOneField(TwitchProfile, on_delete=models.CASCADE, verbose_name="Профиль Twitch",
+                                          blank=True, null=True)
+
+    def __str__(self):
+        return self.tg_profile.name
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+
 class Subscribe(models.Model):
     twitch_profile = models.OneToOneField(TwitchProfile, verbose_name="Twitch профиль", primary_key=True,
                                           on_delete=models.CASCADE)
-    time_end_of_subs = models.DateTimeField(verbose_name="Конец подписки")
+    time_end_of_subs = models.DateTimeField(verbose_name="Конец подписки", blank=True, null=True)
+
+    sub_list = models.ManyToManyField(Profile, verbose_name="Подписчики", related_name="Подписчики", blank=True)
+
+    # Профиль
 
     def __str__(self):
         return "Подписка на " + self.twitch_profile.username
@@ -67,16 +86,3 @@ class Subscribe(models.Model):
     class Meta:
         verbose_name = "Подписка"
         verbose_name_plural = "Подписки"
-
-
-class Profile(models.Model):
-    tg_profile = models.OneToOneField(TelegramProfile, on_delete=models.CASCADE, verbose_name="Профиль Telegram",
-                                      blank=True)
-    vk_profile = models.OneToOneField(VKProfile, on_delete=models.CASCADE, verbose_name="Профиль VK", blank=True)
-    twitch_profile = models.OneToOneField(TwitchProfile, on_delete=models.CASCADE, verbose_name="Профиль Twitch",
-                                          blank=True)
-    subscribe_list = models.ManyToManyField(Subscribe, verbose_name="Список подписок",blank=True)
-
-    class Meta:
-        verbose_name = "Профиль"
-        verbose_name_plural = "Профили"
