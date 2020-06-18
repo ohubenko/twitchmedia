bd881f2df5ccb8d68b86e7d56e85732e444ecb85
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-06-19 00:48:00 +0300

    Try fix

--- website/views.py
+++ website/views.py
@@ -119,7 +119,10 @@ class TwitchWebHookSubscriptions(APIView):
         try:
             tp = TwitchProfile.objects.get(username=streamer)
             content = str(request.GET['hub.challenge'])
+            print(content)
             content.replace('"', '')
-            return Response(content, content_type="text/HTML", status=200)
+            print(content)
+            print(content.encode('utf-8'))
+            return Response(content.encode('utf-8'), content_type="text/plain", status=200)
         except TelegramProfile.DoesNotExist:
             return Response(status=404)
